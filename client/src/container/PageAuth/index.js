52d163b40312b18e6221f2a1b89a227a74a0f479
Avazkhon <kamalxanovavazxon@gmail.com>
2020-01-12 14:34:14 +0700

    wip #1: create auth

--- client/src/container/PageAuth/index.js
+++ client/src/container/PageAuth/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import Auth from '../../components/auth'
+
+import './style.css';
+
 class PageAuth extends React.Component {
   render() {
     return (
       <div className="page-auth">
-        PageAuth
+        <div className="page-auth__connect">
+          <Auth />
+        </div>
       </div>
     );
   }
