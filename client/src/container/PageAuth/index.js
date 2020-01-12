a246f8a8e112c8c086d9a28cf07c74f202e6d795
Avazkhon <kamalxanovavazxon@gmail.com>
2020-01-12 13:20:06 +0700

    wip #1: create PageAuth

---
+++ client/src/container/PageAuth/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PageAuth extends React.Component {
+  render() {
+    return (
+      <div className="page-auth">
+        PageAuth
+      </div>
+    );
+  }
+}
+
+export default PageAuth;
