53b783033624532aafc573c9a7080e3ce9ea50f3
Avazkhon <kamalxanovavazxon@gmail.com>
2020-01-19 02:14:38 +0700

    wip #1: client:  use react-select

--- client/src/components/createNewUser/index.js
+++ client/src/components/createNewUser/index.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
 
 import PropTypes from 'prop-types';
 
 import './style.css';
+
+const optionsMonth = [
+  { value: 'January', label: 'январь' },
+  { value: 'February', label: 'февраль' },
+  { value: 'March', label: 'март' },
+  { value: 'April', label: 'апрель' },
+  { value: 'May', label: 'май' },
+  { value: 'June', label: 'июнь' },
+  { value: 'July', label: 'июль' },
+  { value: 'August', label: 'август' },
+  { value: 'September', label: 'сентябрь' },
+  { value: 'October', label: 'октябрь' },
+  { value: 'November', label: 'ноябрь' },
+  { value: 'December', label: 'декабрь' },
+];
+
+const optionsDay = (() => {
+  const day = [];
+  for (var i = 1; i <= 31; i++) {
+    day.push({ value: i, label: i });
+  }
+  return day;
+})()
+
+const optionsYear = (() => {
+  const year = [];
+  const itYear = new Date().getFullYear();
+  for (var i = itYear - 18; i >= itYear - 100; i--) {
+    year.push({ value: i, label: i });
+  }
+  return year;
+})();
 
 class CreateNewUser extends Component {
   constructor(props) {
@@ -12,6 +45,11 @@ class CreateNewUser extends Component {
         email: '',
         password: '',
         userName: '',
+        age: {
+          month: '',
+          day: '',
+          year: '',
+        }
       },
 		}
 	}
@@ -33,6 +71,48 @@ class CreateNewUser extends Component {
     })
   }
 
+  handleChangeDay = day => {
+    this.setState((prevState) => {
+      return {
+        data: {
+          ...prevState.data,
+          age: {
+            ...prevState.data.age,
+            day
+          }
+        }
+      }
+    });
+  };
+
+  handleChangeMonth = month => {
+    this.setState((prevState) => {
+      return {
+        data: {
+          ...prevState.data,
+          age: {
+            ...prevState.data.age,
+            month
+          }
+        }
+      }
+    });
+  };
+
+  handleChangeYear = year => {
+    this.setState((prevState) => {
+      return {
+        data: {
+          ...prevState.data,
+          age: {
+            ...prevState.data.age,
+            year
+          }
+        }
+      }
+    });
+  };
+
   render() {
     const {
       handleChange,
@@ -40,8 +120,19 @@ class CreateNewUser extends Component {
       handleCreateNewUser,
     } = this.props;
 
-    // const {
-    // } = this.state;
+    const {
+      data: {
+        age: {
+          month,
+          day,
+          year,
+        },
+        email,
+        password,
+        userName,
+      }
+
+    } = this.state;
 
     return(
       <div
@@ -53,6 +144,7 @@ class CreateNewUser extends Component {
             <label className="login__label">
                 <span className="login__label-value">Электронная почта</span>
               <input
+                value={email}
                 onChange={this.handleChange}
                 placeholder="Ввидите электронная почту"
                 className="login__input"
@@ -64,6 +156,7 @@ class CreateNewUser extends Component {
             <label className="login__label">
               <span className="login__label-value">Пароль</span>
               <input
+                value={password}
                 onChange={this.handleChange}
                 placeholder="Ввидите пароль"
                 className="login__input"
@@ -75,6 +168,7 @@ class CreateNewUser extends Component {
             <label className="login__label">
               <span className="login__label-value">Имя</span>
               <input
+                value={userName}
                 onChange={this.handleChange}
                 placeholder="Ввидите имя"
                 className="login__input"
@@ -83,6 +177,35 @@ class CreateNewUser extends Component {
               >
               </input>
             </label>
+            <div className="login__age">
+            <label className="login__label">
+              <span className="login__label-value">День</span>
+                <Select
+                  placeholder="Выбрать"
+                  value={day}
+                  onChange={this.handleChangeDay}
+                  options={optionsDay}
+                />
+            </label>
+            <label className="login__label">
+              <span className="login__label-value">Месяц</span>
+              <Select
+                placeholder="Выбрать"
+                value={month}
+                onChange={this.handleChangeMonth}
+                options={optionsMonth}
+              />
+            </label>
+            <label className="login__label">
+              <span className="login__label-value">Год</span>
+              <Select
+                placeholder="Выбрать"
+                value={year}
+                onChange={this.handleChangeYear}
+                options={optionsYear}
+              />
+            </label>
+            </div>
           </div>
           <div className="login__btn-group">
             <input
