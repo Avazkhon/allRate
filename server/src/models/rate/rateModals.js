b8413031c0477366cacd5d04f01ffc6d1cc1490d
Avazkhon <kamalxanovavazxon@gmail.com>
2020-01-25 18:49:51 +0700

    wip #10: create rateModals

---
+++ server/src/models/rate/rateModals.js
@@ -0,0 +1,35 @@
+const ObjectID = require('mongodb').ObjectID;
+const mongoose = require('mongoose');
+
+const rateSchema = require('./rateSchema');
+
+const Rate = mongoose.model('Users', rateSchema.rateSchema);
+
+exports.postAddOne = (data, callBack) => {
+  const note = new Rate(data);
+  note.save(callBack);
+}
+
+exports.all = (callBack) => {
+  Rate.find({}, callBack);
+}
+
+exports.getOneById = (id, callBack) => {
+  Rate.findOne({_id: id}, callBack);
+}
+
+exports.getOneByAuthot = (authot, callBack) => {
+  Rate.find({ authot }, callBack);
+}
+
+exports.getOneByTitle = (title, callBack) => {
+  Rate.find({ title }, callBack);
+}
+
+exports.updateOne = (id, data, callBack) => {
+  Rate.findByIdAndUpdate({_id: id}, data, callBack);
+}
+
+exports.deleteOne = (id, callBack) => {
+  Rate.deleteOne({_id: id}, callBack);
+}
---
+++ server/src/models/rate/rateSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+exports.rateSchema = new Schema(
+  {
+    title: { type: String, required: true, min: 3, max: 50 },
+    description: { type: String, required: true, min: 10, max: 500 },
+    time: { type: Date, required: true },
+    authot: mongoose.ObjectId,
+    rate: [{
+      name: { type: String, required: true, min: 3, max: 50 },
+      description: { type: String, required: true, min: 10, max: 200 },
+      leval: { type: Number, default: 1 },
+      startingRatio: { type: Number, min: 0.1, max: 100}
+      bidForItem: [{
+        userId: mongoose.ObjectId,
+        meny: { type: Number, required: true, min: 50, max: 500 },
+        time: { type: Date, required: true },
+      }],
+    }],
+  },
+  { collection: 'Rate' }
+);
