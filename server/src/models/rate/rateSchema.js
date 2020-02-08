c51ec2dea027da335bde25d8e7907dd622d1a652
Avazkhon <kamalxanovavazxon@gmail.com>
2020-02-09 03:13:07 +0700

    wip #10: server: change rateSchema

--- server/src/models/rate/rateSchema.js
+++ server/src/models/rate/rateSchema.js
@@ -6,18 +6,25 @@ exports.rateSchema = new Schema(
     title: { type: String, required: true, min: 3, max: 50 },
     description: { type: String, required: true, min: 10, max: 500 },
     time: { type: Date, required: true },
-    author: mongoose.ObjectId,
-    rate: [{
-      name: { type: String, required: true, min: 3, max: 50 },
+    author: { type: mongoose.ObjectId, required: true },
+    reasonsForBetting: [{
+      title: { type: String, required: true, min: 3, max: 50, unique: true },
+      id: { type: String, required: true },
+      statusFictory: { type: Boolean, default: false }
+    }],
+    party: [{
+      participator: { type: String, required: true, min: 3, max: 50 },
       description: { type: String, required: true, min: 10, max: 200 },
       leval: { type: Number, default: 1 },
-      startingRatio: { type: Number, min: 0.1, max: 100},
+      startingRatio: { type: Number, min: -0.01, max: 0.9},
       bidForItem: [{
-        userId: mongoose.ObjectId,
+        userId: { type: mongoose.ObjectId, required: true },
         meny: { type: Number, required: true, min: 50, max: 500 },
         time: { type: Date, required: true },
+        reasonForBid: { type: String, required: true },
       }],
     }],
+    description: String,
   },
   { collection: 'Rate' }
 );
