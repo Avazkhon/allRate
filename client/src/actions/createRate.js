87ec510598623eadac0216112eb925e3d0fc147d
Avazkhon <kamalxanovavazxon@gmail.com>
2020-02-10 00:03:11 +0700

    wip #10: update endpoint "rate"

--- client/src/actions/createRate.js
+++ client/src/actions/createRate.js
@@ -6,13 +6,13 @@ import {
 } from '../constants';
 
 export function creteNewRate (data) {
+  const fullDtat = { ...data, localTime: new Date()}
   return dispatch => dispatch({
     [CALL_API]: {
       types: [  CREATE_NEW_RATE_REQUEST, CREATE_NEW_RATE_SUCCESS, CREATE_NEW_RATE_FAIL],
       method: "POST",
       endpoint: "rate",
-      data
+      data: fullDtat,
     }
-
   })
 }
