c0b6ffb26835f98a3c61788275d41ed2c8fa1c05
Avazkhon <kamalxanovavazxon@gmail.com>
2020-02-27 00:43:30 +0700

    wip #13: client: show rate common

--- client/src/container/RateList/index.js
+++ client/src/container/RateList/index.js
@@ -31,14 +31,63 @@ class RateList extends React.Component {
     return (
       <Layout>
         <div className="rate-list">
-          <div>
-            <ul>
+          <div className="rate-list__hedaer">
+            <span>Список ставок</span>
+          </div>
+          <div className="rate-list__content">
+            <ul className="rate-list__items">
               {
                 rateList.data && rateList.data.map((rate) => {
                   return (
-                    <li key={rate._id}>
-                      <div>
+                    <li
+                      key={rate._id}
+                      className="rate-item"
+                    >
+                      <div className="rate-item_header">
                         <span>{rate.title}</span>
+                      </div>
+                      <div className="rate-item_content">
+                        <div className="rate-item_description">
+                          <div><span>Описание</span></div>
+                          <div className="rate-item_description-text">
+                            <p>{rate.description}</p>
+                          </div>
+                        </div>
+                        <div className="rate-item_party">
+                          <div><span>Список участников</span></div>
+                          <div>
+                            <ul>
+                              {
+                                rate.party.map((itm) => {
+                                  return (
+                                    <li
+                                      key={itm._id}
+                                      className="item-party"
+                                    >
+                                      <div className="item-party_header">
+                                        <span>{rate.title}</span>
+                                      </div>
+                                      <div className="item-party_description">
+                                        <div className="item-party_description-text">
+                                          <p>{rate.description}</p>
+                                        </div>
+                                      </div>
+                                    </li>
+                                  )
+                                })
+                              }
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="rate-item_rates">
+                          <div><span>Список ставок</span></div>
+                          <p>rates</p>
+                        </div>
+                      </div>
+                      <div className="rate-item_footer">
+                        <span className="rate-item_date">{rate.serverTime}</span>
+                        <span className="rate-item_date">{rate.dateStart}</span>
+                        <span className="rate-item_date">{rate.dateFinish}</span>
                       </div>
                     </li>
                   );
