4876eee8a00754b8499083fd22c66aafaeadd719
Avazkhon <kamalxanovavazxon@gmail.com>
2020-03-04 20:50:46 +0700

    done #12 fix bug aut and connect

--- server/src/db.js
+++ server/src/db.js
@@ -2,16 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 
 const passwords = require('../password');
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+
+  poolSize: 10, // Maintain up to 10 socket connection
+};
 
 const uri = `mongodb+srv://Avazkhon:${passwords.passwordMongoDB}@cluster0-sgdif.mongodb.net/allRate?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 async function connect(done) {
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }).then(() => {
+  await mongoose.connect(uri, options).then(() => {
       var db = mongoose.connection;
       db.on('error', console.error.bind(console, 'connection error:'));
       done();
