cd8720388bb504604d006a54cbcac8826d574ae0
Avazkhon <kamalxanovavazxon@gmail.com>
2020-03-31 00:29:29 +0700

    wip #23: server: update user model

--- server/src/models/user/index.js
+++ server/src/models/user/index.js
@@ -25,7 +25,7 @@ exports.getOneByUserEmail = (email, callBack) => {
 }
 
 exports.updateOne = (id, data, callBack) => {
-  User.findByIdAndUpdate({_id: id}, data, callBack);
+  User.findByIdAndUpdate({_id: id}, data, { new: true }, callBack);
 }
 
 exports.deleteOne = (id, callBack) => {
