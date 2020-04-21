3f1274e921a31683ceccbeec8b036fdbc94d8dff
Avazkhon <kamalxanovavazxon@gmail.com>
2020-04-21 18:25:54 +0700

    wip #27: change schema rate

--- server/src/models/rate/rateSchema.js
+++ server/src/models/rate/rateSchema.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const url = 'https://sun9-39.userapi.com/c852216/v852216813/1239e2/VZL0QayR6E4.jpg?ava=1';
+const urlParty = 'https://sun9-39.userapi.com/c852216/v852216813/1239e2/VZL0QayR6E4.jpg?ava=1';
+const urlMain = 'https://obuchonok.ru/files/images/kibersport_0.jpg';
 
 exports.rateSchema = new Schema(
   {
@@ -12,35 +13,55 @@ exports.rateSchema = new Schema(
     differenceTime: { type: Number, required: true },
     localTime: { type: Date, required: true },
     dateStart: { type: Date, required: true },
-    dateFinish: { type: Date, required: true },
+    dateFinish: { type: Date },
+    dateAlert: { type: Date },
     statusLife: { type: String, default: 'new' },
-    reasonsForBetting: [
-      {
-        title: { type: String, required: true, min: 3, max: 50 },
-        idParty: { type: String, required: true },
-        idRFB: { type: String, required: true },
-        relevant: { type: Boolean, default: true },
+    img: { type: String, default: urlMain },
+    mainBet: {
+      title: { type: String, default: 'Основная ставка' },
+      partyOne: {
         coefficient: { type: Number, default: 1.9, min: 1},
-        img: { type: String, default: url },
-        bidForItem: [
+        idParty: { type: String, required: true },
+        participants: [
           {
             userId: { type: mongoose.ObjectId, required: true },
             meny: { type: Number, required: true, min: 50, max: 500 },
             serverTime: { type: Date, default: new Date() },
             localTime: { type: Date, required: true },
-            reasonForBid: { type: String, required: true },
-            makeCoefficient: { type: Number, default: 1.9, min: 1},
           }
         ],
-      }
-    ],
+      },
+      partyDraw: {
+        coefficient: { type: Number },
+        idParty: { type: String },
+        participants: [
+          {
+            userId: { type: mongoose.ObjectId },
+            meny: { type: Number, min: 50, max: 500 },
+            serverTime: { type: Date },
+            localTime: { type: Date },
+          }
+        ],
+      },
+      partyTwo: {
+        coefficient: { type: Number, default: 1.9, min: 1},
+        idParty: { type: String, required: true },
+        participants: [
+          {
+            userId: { type: mongoose.ObjectId, required: true },
+            meny: { type: Number, required: true, min: 50, max: 500 },
+            serverTime: { type: Date, default: new Date() },
+            localTime: { type: Date, required: true },
+          }
+        ],
+      },
+    },
     party: [{
       id: { type: String, required: true },
       participator: { type: String, required: true, min: 3, max: 50 },
       description: { type: String, required: true, min: 10, max: 200 },
-      leval: { type: Number, default: 1 },
+      img: { type: String, default: urlParty },
     }],
-    description: String,
   },
   { collection: 'Rate' }
 );
