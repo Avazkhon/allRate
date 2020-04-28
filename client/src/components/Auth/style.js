caac361c96a3ea7714e747eecfcc1335b2e8e447
Avazkhon <kamalxanovavazxon@gmail.com>
2020-04-28 20:43:59 +0700

    wip #30: change class to function

--- client/src/components/Auth/style.js
+++ client/src/components/Auth/style.js
@@ -7,5 +7,11 @@ export default {
     padding:' 10px',
     'background-color': '#343a40!important',
     'z-index': 10
+  },
+
+  '@media (max-width: 1000px)': {
+    auth: {
+      'margin': '50px auto',
+    },
   }
 };
