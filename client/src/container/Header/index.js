01e268d66e2eb97d968d943d4b1975af6b9df767
Avazkhon <kamalxanovavazxon@gmail.com>
2020-04-28 23:37:26 +0700

    wip #30: delete Link

--- client/src/container/Header/index.js
+++ client/src/container/Header/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
-import { Link } from "react-router-dom";
 import injectSheet from 'react-jss';
 
 import {
