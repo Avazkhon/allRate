3ed3c5d0c6946043af1304409ec952f6f94531a2
Avazkhon <kamalxanovavazxon@gmail.com>
2020-05-10 01:48:10 +0700

    wip #36:client: update CardPost

--- client/src/components/CardPost/index.js
+++ client/src/components/CardPost/index.js
@@ -7,22 +7,44 @@ import {
   Button,
 } from 'react-bootstrap';
 
-const CardRate = () => {
+const CardRate = ({
+  post: {
+    title,
+    text,
+    _id,
+    img,
+    views,
+  }
+}) => {
   return (
     <Card>
-      <Card.Header>Featured</Card.Header>
+      <Card.Img variant="top" src={img.url} />
       <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{text}</Card.Text>
+        views: {views}
+      </Card.Body>
+      <Card.Body>
+        <Card.Link
+          href={`make-rate?rateId=${_id}`}
+        >
+          Перейти
+        </Card.Link>
       </Card.Body>
     </Card>
   )
 }
 
 CardRate.propType = {
+  post: PropTypes.shape({
+    title: PropTypes.string,
+    text: PropTypes.string,
+    _id: PropTypes.string,
+    img: PropTypes.shape({
+      url: PropTypes.string,
+    }),
+    views: PropTypes.number,
+  })
 }
 
-export default CardRate
+export default CardRate;
