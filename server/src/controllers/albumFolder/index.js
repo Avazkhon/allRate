2cf9645a010169fbc11a0507463752010b6054d4
Avazkhon <kamalxanovavazxon@gmail.com>
2020-06-20 00:36:06 +0700

    wip #38:server: add img for rate and post

--- server/src/controllers/albumFolder/index.js
+++ server/src/controllers/albumFolder/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
 const userModels = require('../../models/user');
+const postModels = require('../../models/post');
+const rateModels = require('../../models/rate');
 
 class AlbumFolder {
   constructor () {
@@ -29,22 +31,72 @@ class AlbumFolder {
     })
   }
 
+  changePostImg = (fileUploaded, postId) => {
+    const nameImage = `${postId}-${fileUploaded.name.replace(/ /g,"_")}`
+    return this.addFile(fileUploaded, this.PATH, postId)
+    .then(() => {
+      postModels.findByIdAndUpdate({ _id: postId },
+        {'$set': {
+            'img.url': `/api/img/?idImg=${nameImage}`,
+          }
+        }
+      );
+    })
+  }
+
+  changeRateImg = (fileUploaded, rateId) => {
+    const nameImage = `${rateId}-${fileUploaded.name.replace(/ /g,"_")}`
+    return this.addFile(fileUploaded, this.PATH, rateId)
+    .then(() => {
+      rateModels.findByIdAndUpdate({ _id: rateId },
+        {'$set': {
+            'img': `/api/img/?idImg=${nameImage}`,
+          }
+        }
+      );
+    })
+  }
+
+  response = () => {
+
+  }
+
   addImage = async (req, res) => {
     const { user } = req.session;
+    const { post, rate } = req.query;
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).json({ err: 'No files were uploaded.' });
     } else if (!user || !user.userId) {
       return res.status(401).json({ err: 'Пользователь не авторизован!' });
     }
 
-    const fileUploaded = req.files.fileUploaded;
-    this.changeAvatar(fileUploaded, user.userId)
-    .then((result) => {
-      res.status(200).json({ message: 'Новое фото добавлено!'});
-    })
-    .catch((err) => {
-      res.status(500).json({ err: err.toString() });
-    })
+    if (req.files.avatar) {
+      this.changeAvatar(req.files.avatar, user.userId)
+      .then((result) => {
+        res.status(200).json({ message: 'Новое фото добавлено!'});
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.toString() });
+      })
+    } else if (req.files.post && post) {
+      this.changePostImg(req.files.post, post)
+      .then((result) => {
+        res.status(200).json({ message: 'Новое фото добавлено!'});
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.toString() });
+      })
+    } else if (req.files.rate && rate) {
+      this.changeRateImg(req.files.rate, rate)
+      .then((result) => {
+        res.status(200).json({ message: 'Новое фото добавлено!'});
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.toString() });
+      })
+    } else {
+      res.status(400).json({ err: 'запрос не корректен' });
+    }
   }
 
   getImg = (req, res) => {
