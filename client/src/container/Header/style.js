dddc2a308a8239baf1558089c792b5f24260ac35
Avazkhon <kamalxanovavazxon@gmail.com>
2020-08-09 00:01:49 +0700

    wip #48:client: change nav links

--- client/src/container/Header/style.js
+++ client/src/container/Header/style.js
@@ -2,4 +2,16 @@ export default {
   'header__auth': {
     position: 'relative',
   },
+
+  'nav-link': {
+    color: 'white',
+    'margin-left': '10px',
+    'text-decoration': 'none',
+
+    '&:hover': {
+      color: 'white',
+      opacity: .7,
+      'text-decoration': 'none',
+    }
+  }
 }
