65bc68da7b6d81748525d06d0670cc2a9eb2f132
Avazkhon <kamalxanovavazxon@gmail.com>
2020-08-18 22:25:48 +0700

    wip #48:server: add comment

--- server/src/models/user/index.js
+++ server/src/models/user/index.js
@@ -5,27 +5,27 @@ class UserModal extends CreateModel {
 
   checkPassword = (getParams = {}) => {
     // эта функция позволяет защитить пароли и не выдает их без потребности
-    let params = {};
-    if (getParams.password) {
-      delete getParams.password;
-      params = { ...getParams };
-    } else {
-      params = { ...getParams, password: 0 };
-    }
-    return params
+    // let params = {};
+    // if (getParams.password) {
+    //   delete getParams.password;
+    //   params = { ...getParams };
+    // } else {
+    //   params = { ...getParams, password: 0 };
+    // }
+    return getParams
   }
 
   async findOne (searchParams, getParams) {
     return super.findOne(searchParams, this.checkPassword(getParams));
   }
 
-  async findByIdAndUpdate (searchParams, data, addParms) {
-    return super.findByIdAndUpdate(searchParams, this.checkPassword(data), addParms)
-  }
-
-  async findOneAndUpdate (searchParams, data, addParms) {
-    return super.findOneAndUpdate(searchParams, this.checkPassword(data), addParms)
-  }
+  // async findByIdAndUpdate (searchParams, data, addParms) {
+  //   return super.findByIdAndUpdate(searchParams, this.checkPassword(data), addParms)
+  // }
+  //
+  // async findOneAndUpdate (searchParams, data, addParms) {
+  //   return super.findOneAndUpdate(searchParams, this.checkPassword(data), addParms)
+  // }
 
   async getByProps (props, params) {
     return super.getByProps(props, this.checkPassword(params))
