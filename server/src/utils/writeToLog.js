3da6475203fcd38998f15c9b4d424e01771347a5
Avazkhon <kamalxanovavazxon@gmail.com>
2020-10-27 00:59:12 +0700

    wip #51:server: write log in console in not production

--- server/src/utils/writeToLog.js
+++ server/src/utils/writeToLog.js
@@ -11,6 +11,14 @@ function WriteToLog () {
     if (count > 1) {
       return
     }
+    if (count === 0 && process.env.NODE_ENV !== 'production') {
+      console.group("process in");
+      console.log('____________________');
+      console.log(folder + fileName);
+      console.log(data);
+      console.log('____________________');
+      console.groupEnd();
+    }
     count++;
 
     fs.appendFile(folder + fileName, `\n${new Date}: ${data}`, (err) => {
