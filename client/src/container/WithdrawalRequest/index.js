c1166eeba5626bfc6087c66bb3d690f8204d2a44
Avazkhon <kamalxanovavazxon@gmail.com>
2020-11-04 03:03:31 +0700

    wip #51:client: add modal

--- client/src/container/WithdrawalRequest/index.js
+++ client/src/container/WithdrawalRequest/index.js
@@ -9,6 +9,9 @@ import {
   Button,
   Dropdown,
   Table,
+  Modal,
+  ListGroup,
+  Form,
 } from 'react-bootstrap';
 
 import {
@@ -24,6 +27,7 @@ class Purse extends Component {
     this.state = {
       id: null,
       description: '',
+      isShowModal: null,
     }
   }
 
@@ -60,9 +64,23 @@ class Purse extends Component {
     this.submitWR(description, name, id);
   }
 
+  handleChange = (e) => {
+    const value = e.currentTarget.value;
+    this.setState({description: value})
+  }
+
+  handleShow = (e) => {
+    const id = e.currentTarget.dataset.id;
+    this.setState({isShowModal: id})
+  }
+
+  handleClose = () => {this.setState({isShowModal: null})}
+
   render() {
     const {
       id,
+      isShowModal,
+      description,
     } = this.state;
     const {
       auth: {
@@ -103,16 +121,7 @@ class Purse extends Component {
                         <td>{WR.amount_due}</td>
                         <td>{WR._id === id ? 'Загрузка' : WR.status}</td>
                         <td>
-                          <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                              Действия
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                              <Dropdown.Item><Button variant="primary" data-id={WR._id} name="successfully" onClick={this.handleMake}>Успешный перевод</Button></Dropdown.Item>
-                              <Dropdown.Item><Button variant="primary" data-id={WR._id} name="refused" onClick={this.handleMake}>Не успешный перевод</Button></Dropdown.Item>
-                            </Dropdown.Menu>
-                          </Dropdown>
+                          <Button variant="primary" data-id={WR._id} onClick={this.handleShow}>Выполнить</Button>
                         </td>
                       </tr>
                       )
@@ -120,6 +129,35 @@ class Purse extends Component {
                 }
               </tbody>
             </Table>
+              <Modal show={!!isShowModal} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Выберите действия</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  {
+                    docs.map((WR) => {
+                      if (WR._id !== isShowModal) {
+                        return '';
+                      }
+                      return (
+                        <ListGroup>
+                          <ListGroup.Item>{WR.target}</ListGroup.Item>
+                          <ListGroup.Item>{WR.amount}</ListGroup.Item>
+                          <ListGroup.Item>{WR.amount_due}</ListGroup.Item>
+                          <ListGroup.Item>{WR._id === id ? 'Загрузка' : WR.status}</ListGroup.Item>
+                        </ListGroup>
+                      )
+                    })
+                  }
+                  <Form.Group>
+                    <Form.Control size="lg" type="text" placeholder="Ввидите описания" value={description} onChange={this.handleChange}/>
+                  </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="primary" data-id={isShowModal} name="successfully" onClick={this.handleMake}>Успешный перевод</Button>
+                  <Button variant="danger" data-id={isShowModal} name="refused" onClick={this.handleMake}>Не успешный перевод</Button>
+                </Modal.Footer>
+              </Modal>
             </Col>
           </Row>
         </Container>
