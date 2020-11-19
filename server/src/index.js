3a1e53e3361f2097a65a647cc25324a53423e3a8
Avazkhon <kamalxanovavazxon@gmail.com>
2020-11-20 01:17:07 +0700

    wip #55:server: create routes

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ const MongoStore = require('connect-mongo')(session);
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 
+const routes = require('./routes');
 const WriteToLog = require('./utils/writeToLog');
 const InnerTask = require('./innerTask');
 
@@ -13,29 +14,11 @@ require('./server.conf.js').addConf();
 
 const db = require('./db');
 
-const rateControllers = require('./controllers/rateControllers');
-const langControllers = require('./controllers/langControllers');
-const postControllers = require('./controllers/post');
-const rateLiveControllers = require('./controllers/rateControllers/rateLive');
-const userControllers = require('./controllers/user');
-const authControllers = require('./controllers/auth');
-const purseControllers = require('./controllers/purse');
-const subscriptionsControllers = require('./controllers/subscriptions');
-const ratingControllers = require('./controllers/rating');
-const viewsControllers = require('./controllers/views');
-const withdrawalRequest = require('./controllers/withdrawalRequest');
-const InvoiceControllers = require('./controllers/invoice');
-const AlbumFolder = require('./controllers/albumFolder');
-const withdrawalRequestAdmin = require('./controllers/withdrawalRequest/withdrawalRequestAdmin');
-
 const passwords = require('../password');
 
 const app = express();
-const router = express.Router();
 const writeToLog = new WriteToLog();
 const innerTask = new InnerTask();
-const invoiceControllers = new InvoiceControllers();
-const albumFolder = new AlbumFolder();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -44,12 +27,12 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(session({
   secret: passwords.secret,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  saveUninitialized: false,
+  resave: true,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 3 },
   store: new MongoStore({
     mongooseConnection: mongoose.connection,
-    ttl: 1000 * 60 * 60 * 24
+    ttl: 1000 * 60 * 60 * 24 * 3
   }),
 }));
 app.use(fileUpload());
@@ -65,76 +48,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/api/', (req, res) => {
-  res.send('Hello! welcome to the "All rate"!');
-})
-
-app.route('/api/auth')
-  .get(authControllers.authAut)
-  .post(authControllers.authIn);
-
-app.patch('/api/mail_confirmation', authControllers.mailConfirmation);
-
-app.route('/api/subscription')
-  .get(subscriptionsControllers.get)
-  .put(subscriptionsControllers.addSubscription)
-  .delete(subscriptionsControllers.deleteSubscription);
-
-app.route('/api/lang')
-  .get(langControllers.get)
-  .post(langControllers.post);
-
-app.route('/api/post')
-  .get(postControllers.get)
-  .put(postControllers.put)
-  .post(postControllers.create)
-  .delete(postControllers.deleteOne);
-
-app.patch('/api/rating', ratingControllers.rating)
-app.patch('/api/views', viewsControllers.views)
-
-app.route('/api/user')
-  .get(userControllers.getUser) // обрабатывает запросы по userName, id и all
-  .post(userControllers.craeteUser)
-  .put(userControllers.updateOne)
-  .delete(userControllers.deleteOne);
-
-
-app.route('/api/rate')
-  .get(rateControllers.getRate)
-  .post(rateControllers.postAddOne)
-  .put(rateControllers.findByIdAndUpdate)
-  .delete(rateControllers.deleteOne)
-
-app.route('/api/rate-live')
-  .put(rateLiveControllers.rateLive)
-
-app.route('/api/purse')
-  .get(purseControllers.getPurse)
-  // этот метод не должен быть доступным на production
-  // так как пользователь не должен иметь примой доступ к нему
-  .put(purseControllers.findByIdAndUpdate);
-
-app.route('/api/invoice')
-  .get(invoiceControllers.getInvoice)
-  .post(invoiceControllers.createInvoice)
-
-app.route('/api/image')
-  .post(albumFolder.addImage)
-
-app.get('/api/img', albumFolder.getImg);
-
-app.route('/api/withdrawal-request')
-  // .get(withdrawalRequest.get)
-  .post(withdrawalRequest.create)
-
-app.route('/api/admin/withdrawal-request')
-  .get(withdrawalRequestAdmin.get)
-  .patch(withdrawalRequestAdmin.patch);
-
-app.route('/api/password-recovery/userPassword')
-  .post(userControllers.passwordRecoveryStart)
-  .put(userControllers.passwordRecoveryFinish)
+routes(app);
 
 db.connect((err) => {
   if (err) {
