019ac1704274dc37438d924bfeb34b19cd2a3664
Avazkhon <kamalxanovavazxon@gmail.com>
2020-12-29 23:45:26 +0700

    wip #60:server: chech is paymentMade

--- server/src/controllers/rateControllers/paymentAfterRate.js
+++ server/src/controllers/rateControllers/paymentAfterRate.js
@@ -47,14 +47,22 @@ class PaymentAfterRate {
         return blocks
       } else {
         if (blocks.blocks[blocksIndex].type === typeBlock.total) {
-          await this.makePaymentTotal(blocks.blocks[blocksIndex])
-          blocks.blocks[blocksIndex].paymentMade = true
+
+          if (!blocks.blocks[blocksIndex].paymentMade) {
+            await this.makePaymentTotal(blocks.blocks[blocksIndex])
+            blocks.blocks[blocksIndex].paymentMade = true
+          }
+
           blocksIndex++
           return this.makePaymentBlocks(blocks, blocksIndex)
         }
         if (blocks.blocks[blocksIndex].type === typeBlock.boolean) {
-          await this.makePaymentBoolean(blocks.blocks[blocksIndex])
-          blocks.blocks[blocksIndex].paymentMade = true
+
+          if (!blocks.blocks[blocksIndex].paymentMade) {
+            await this.makePaymentBoolean(blocks.blocks[blocksIndex])
+            blocks.blocks[blocksIndex].paymentMade = true
+          }
+
           blocksIndex++
           return this.makePaymentBlocks(blocks, blocksIndex)
         }
@@ -72,9 +80,12 @@ class PaymentAfterRate {
       console.log('finish makePaymentTotal');
      return block
     } else if (block.bets[indexbBet].win) {
-        block.bets[indexbBet].participants
-        await this.makePaymentTotalParticipants(block.bets[indexbBet].participants, block.bets[indexbBet].coefficient)
-        block.bets[indexbBet].paymentMade = true
+
+        if (!block.bets[indexbBet].paymentMade) {
+          await this.makePaymentTotalParticipants(block.bets[indexbBet].participants, block.bets[indexbBet].coefficient)
+          block.bets[indexbBet].paymentMade = true
+        }
+
         indexbBet++
         return this.makePaymentTotal(block, indexbBet)
     } else {
@@ -89,14 +100,18 @@ class PaymentAfterRate {
           console.log('finish makePaymentTotalParticipants');
          return participants
        } else {
-           const dataInvoice = {
-             amount: Math.floor(participants[participantsIndex].amount * coefficient),
-             requisites: { src: this.rate.purseId, target: participants[participantsIndex].purseId},
-           };
-           await invoiceControllers.createInvoiceForWin(dataInvoice)
-           participants[participantsIndex].paymentMade = true;
-           participantsIndex++
-           return this.makePaymentTotalParticipants(participants, coefficient, participantsIndex)
+
+          if (!participants[participantsIndex].paymentMade) {
+            const dataInvoice = {
+              amount: Math.floor(participants[participantsIndex].amount * coefficient),
+              requisites: { src: this.rate.purseId, target: participants[participantsIndex].purseId},
+            };
+            await invoiceControllers.createInvoiceForWin(dataInvoice)
+            participants[participantsIndex].paymentMade = true;
+          }
+
+          participantsIndex++
+          return this.makePaymentTotalParticipants(participants, coefficient, participantsIndex)
        }
     } catch (e) {
         console.log('e', e);
@@ -113,8 +128,10 @@ class PaymentAfterRate {
         console.log('finish makePaymentBoolean');
         return block
       } else {
-        await this.makePaymentBooleanParticipants(block.bets[indexbBet]);
-        block.bets[indexbBet].paymentMade = true
+        if(!block.bets[indexbBet].paymentMade) {
+          await this.makePaymentBooleanParticipants(block.bets[indexbBet]);
+          block.bets[indexbBet].paymentMade = true
+        }
         indexbBet++
         return this.makePaymentBoolean(block, indexbBet)
       }
@@ -130,7 +147,7 @@ class PaymentAfterRate {
         console.log('finish makePaymentBooleanParticipants');
         return bets;
       } else {
-        if (bets.participants[participantsIndex].noOrYes === bets.noOrYes) {
+        if ((bets.participants[participantsIndex].noOrYes === bets.noOrYes) && (!bets.participants[participantsIndex].paymentMade)) {
           const coefficient = bets.noOrYes ? bets.coefficientYes : bets.coefficientNo;
 
           const dataInvoice = {
