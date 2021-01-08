89a4bc3e70b96a316a678f895f52dfe356b77f5a
Avazkhon <kamalxanovavazxon@gmail.com>
2021-01-08 21:26:31 +0700

    wip #60:server: add betSchema

--- server/src/models/block/blockSchema.js
+++ server/src/models/block/blockSchema.js
@@ -2,6 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const moment = require('moment-timezone');
 
+const betSchema = new Schema(
+  {
+    id: { type: Number, required: true },
+    coefficientNo: { type: Number, min: 1 },
+    coefficientYes: { type: Number, min: 1 },
+    coefficient: { type: Number, min: 1},
+    amountNo: { type: Number, default: 0  },
+    amountYes: { type: Number, default: 0 },
+    amount: { type: Number, default: 0 },
+    noOrYes: { type: Boolean },
+    win: { type: Boolean },
+    condition: { type: String, required: true },
+    createDate: { type: Date, default: moment().utc().format() },
+    paymentMade: { type: Boolean },
+    participants: [
+      {
+        userId: { type: mongoose.ObjectId, required: true },
+        purseId: { type: mongoose.ObjectId, required: true },
+        amount: { type: Number, required: true, min: 50, max: 500 },
+        paymentMade: { type: Boolean },
+        noOrYes: { type: Boolean }, // if type block boolean
+        createDate: { type: Date, default: moment().utc().format() },
+      }
+    ],
+  },
+  {
+    strict: true
+  }
+);
+
 exports.blockSchema = new Schema({
     rateId: { type: mongoose.ObjectId },
     paymentPercentage: { type: Boolean },
@@ -21,34 +51,9 @@ exports.blockSchema = new Schema({
         type: { type: String, required: true }, // boolean, total
         status: { type: String, default: 'disabled' }, // hidden, disabled, active, finish
         amountAll: { type: Number, default: 0  },
-        bets: [
-          {
-            id: { type: Number, required: true },
-            coefficientNo: { type: Number, min: 1 },
-            coefficientYes: { type: Number, min: 1 },
-            coefficient: { type: Number, min: 1},
-            amountNo: { type: Number, default: 0  },
-            amountYes: { type: Number, default: 0 },
-            amount: { type: Number, default: 0 },
-            noOrYes: { type: Boolean },
-            win: { type: Boolean },
-            condition: { type: String, required: true },
-            createDate: { type: Date, default: moment().utc().format() },
-            paymentMade: { type: Boolean },
-            participants: [
-              {
-                userId: { type: mongoose.ObjectId, required: true },
-                purseId: { type: mongoose.ObjectId, required: true },
-                amount: { type: Number, required: true, min: 50, max: 500 },
-                paymentMade: { type: Boolean },
-                noOrYes: { type: Boolean }, // if type block boolean
-                createDate: { type: Date, default: moment().utc().format() },
-              }
-            ],
-          }
-        ]
+        bets: [ betSchema ]
       }
     ]
   },
-  { collection: 'Block' }
+  { collection: 'Block', strict: true }
 );
