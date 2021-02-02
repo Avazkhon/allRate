7c07a8cdbff830c966651f2806ccbdebbeac0689
Avazkhon <kamalxanovavazxon@gmail.com>
2021-02-03 00:55:00 +0700

    wip #68:client: add inputs

--- client/src/container/WorkWithSupport/index.js
+++ client/src/container/WorkWithSupport/index.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import queryString from 'query-string';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+  Button,
+} from '@material-ui/core';
 
 import {
   Grid
@@ -10,15 +19,29 @@ import {
 import {
   getSupportByID,
 } from 'actions';
+
 import CardSupport from 'components/CardSupport';
 
 import Layout from '../Layout';
+
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function WorkWithSupport({
   getSupportByID,
   history,
 }) {
+  const classes = useStyles()
   const [ support, setSupport ] = useState({})
+  const [ comment, setComment ] = useState('')
 
 
   useEffect(() => {
@@ -31,6 +54,16 @@ function WorkWithSupport({
       })
   }, [])
 
+  function handleChangeStatus(e) {
+    const { value } = e.target;
+    setSupport({ ...support, status: value })
+  }
+
+  function changeComment(e) {
+    const { value } = e.target;
+    setComment(value)
+  }
+
   return (
     <Layout>
       <Grid
@@ -41,11 +74,42 @@ function WorkWithSupport({
       >
         <Grid item >
           <h1>Обращение</h1>
-          {
             <CardSupport
               support={support}
             />
-          }
+          </Grid>
+          <Grid item >
+            <FormControl className={classes.formControl}>
+             <InputLabel id="demo-simple-select-label">Статус</InputLabel>
+             <Select
+               labelId="demo-simple-select-label"
+               id="demo-simple-select"
+               value={support.status || ''}
+               onChange={handleChangeStatus}
+             >
+
+               <MenuItem value={'new'}>Новый</MenuItem>
+               <MenuItem value={'skip'}>Пропустить</MenuItem>
+               <MenuItem value={'in_progress'}>В работе</MenuItem>
+               <MenuItem value={'resolved'}>Решен</MenuItem>
+               <MenuItem value={'reject'}>Не решен</MenuItem>
+             </Select>
+           </FormControl>
+         </Grid>
+         <Grid item >
+           <TextField
+              id="outlined-multiline-static"
+              label="Multiline"
+              multiline
+              onChange={changeComment}
+              value={comment}
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item >
+            <Button variant="contained" color="primary">
+              Сохранить комментарии
+            </Button>
         </Grid>
       </Grid>
     </Layout>
