35c341b18e30fe7343eda225d601465aa9a304ab
Avazkhon <kamalxanovavazxon@gmail.com>
2021-05-20 02:43:08 +0700

    change  conf nodemailer

--- server/src/utils/transporter.js
+++ server/src/utils/transporter.js
@@ -4,9 +4,7 @@ const { emailAccount } = require("../../password");
 
 let transporter = nodemailer.createTransport(
   {
-    host: "smtp.yandex.ru",
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
       user: emailAccount.user, // generated ethereal user
       pass: emailAccount.pass, // generated ethereal password
