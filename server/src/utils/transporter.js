3ef89cba38152b09cd6a6cdf3af156cfc41ce596
Avazkhon <kamalxanovavazxon@gmail.com>
2021-06-07 00:51:02 +0700

    change title email frim

--- server/src/utils/transporter.js
+++ server/src/utils/transporter.js
@@ -11,7 +11,7 @@ let transporter = nodemailer.createTransport(
     },
   },
   {
-    from: `"All Rate" <${emailAccount.user}>`, // sender address
+    from: `"FaceBetting" <${emailAccount.user}>`, // sender address
   }
 );
 
