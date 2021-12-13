08a308b2aa9a9c3f8bfa1454b39e536c4bdaf8bf
Avazkhon <kamalxanovavazxon@gmail.com>
2021-12-14 00:11:41 +0700

    update image list

--- client/src/widgets/ImageGridList/index.js
+++ client/src/widgets/ImageGridList/index.js
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
+import { CardMedia } from '@material-ui/core';
 
 import { OptionImage } from './OptionImage';
 
@@ -16,32 +16,33 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '100%',
     display: 'flex',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   },
   imageItem: {
     margin: '8px',
     '& img': {
-      borderRadius: 4
+      borderRadius: 4,
     },
   },
-  img: {
-    width: '100%'
-  }
+  // img: {
+  //   width: '100%',
+  // },
 }));
-
 
 export default function ImageGridList({ tileData, onSelectImageFromAlbums }) {
   const classes = useStyles();
 
   function geSrc(id) {
-    const resize = '?resize=300x200';
-    const src = `/media/image/${id}`
-    return { fullSrc: src + resize, src }
+    // const resize = '?resize=300x200';
+    const src = `/media/image/${id}`;
+    return { fullSrc: src, src };
   }
 
   function getProtocolAndDomain() {
     if (typeof location !== 'undefined') {
-      return `${location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`
+      return `${location.protocol}//${window.location.hostname}${
+        window.location.port ? `:${window.location.port}` : ''
+      }`;
     }
   }
 
@@ -50,28 +51,38 @@ export default function ImageGridList({ tileData, onSelectImageFromAlbums }) {
       accumulator.push(value); // почемуто не работает обычный revesw
       return accumulator;
     }, []);
-  }, [tileData])
+  }, [tileData]);
 
   return (
     <div className={classes.root}>
       <div className={classes.imageList}>
-        {
-          newTileData.map((tile) => {
-            const { fullSrc, src } = geSrc(tile.id);
-            const url = getProtocolAndDomain();
-            return (
-              <div key={tile._id} className={classes.imageItem}>
-                <OptionImage tile={tile} src={url + src} onSelectImageFromAlbums={onSelectImageFromAlbums}>
-                  <img
-                    src={fullSrc}
-                    alt={tile.name || tile}
-                    className={classes.img}
-                  />
-                </OptionImage>
-              </div>
-            )
-          })
-        }
+        {newTileData.map((tile) => {
+          const { fullSrc, src } = geSrc(tile.id);
+          const url = getProtocolAndDomain();
+          return (
+            <div key={tile._id} className={classes.imageItem}>
+              <OptionImage
+                tile={tile}
+                src={url + src}
+                onSelectImageFromAlbums={onSelectImageFromAlbums}
+              >
+                {/* <img
+                  src={fullSrc}
+                  alt={tile.name || tile}
+                  className={classes.img}
+                /> */}
+                <CardMedia
+                  component="img"
+                  height="300"
+                  width="300"
+                  image={`/media/image/${fullSrc}`}
+                  // className={classes.img}
+                  alt={tile.name || tile}
+                />
+              </OptionImage>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
